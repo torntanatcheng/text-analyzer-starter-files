c76f6615d24c60867c8398fcdb1181fd34eb7796
Jump Cheng <torntanat.cheng@gmail.com>
2017-01-09 13:00:43 +0700

    almost complete solution

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-// your code here!
+$(document).ready(function() {
+//JavaScript
+function wordCount(text){
+	var splitWord = text.split(" ");
+	var array = [];
+	for (var i = 0; i < splitWord.length; i++) {
+		array.push(splitWord[i]);
+	}
+	return array.length;
+}
+
+
+function uniqueWord(text) {
+	var count = [];
+	for (var i = 0; i <= text.length; i++) {
+		if (count.indexOf(text[i]) === -1) {
+			count.push(text[i]);
+		}
+	}
+	return count.length;
+}
+
+function averageWordLength (text){
+  var totalLength = text.join("").length;
+  return (totalLength / text.length).toFixed(2);
+}
+
+//jQuery
+
+function reportText(text) {
+
+	var textReport = $('.js-text-report');
+
+	//Function Results
+	var totalWordCount = wordCount(text);
+	var uniqueWordCount = uniqueWord(text);
+	var avgWordLength = averageWordLength(text);
+
+	textReport.find('.js-word-count').text(totalWordCount);
+	textReport.find('.js-unique-word-count').text(uniqueWordCount);
+	textReport.find('.js-average-word-length').text(avgWordLength);
+	textReport.removeClass('hidden');
+}
+
+	$('.js-text-form').on('submit', function(event){
+		event.preventDefault();
+		var userInput = $(this).find('#user-text').val();
+		reportText(userInput);
+	});
+
+});
